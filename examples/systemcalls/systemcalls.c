233e00e998ba6e715ff44de7845da4f079287bde
Allef Pablo Araujo <allefpablo@gmail.com>
2022-12-20 23:08:04 -0300

    Assignment 3 Part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +21,9 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret = system(cmd);
+
+    if (ret != 0) return false;
 
     return true;
 }
@@ -58,6 +66,21 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
+    pid_t pid;
+
+    pid = fork();
+    if (pid == -1)
+	    return false;
+    else if (pid == 0) {
+    	execv(command[0], command);
+	exit(-1);
+    }
+
+    if (waitpid(pid, &status, 0) == -1) 
+	    return false;
+    else if (WIFEXITED(status) && (WEXITSTATUS(status) != 0))
+	    return false;
 
     va_end(args);
 
@@ -92,6 +115,31 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int status;
+    pid_t pid;
+    
+    pid = fork();
+    
+    if (pid == -1)
+	    return false;
+    else if (pid == 0) {
+	
+	int fd = open(outputfile, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd < 0) {
+		return false;
+	}
+	dup2(fd, 1);
+	dup2(fd, 2);
+	close(fd);
+
+    	execv(command[0], command);
+	exit(-1);
+    }
+
+    if (waitpid(pid, &status, 0) == -1) 
+	    return false;
+    else if (WIFEXITED(status) && (WEXITSTATUS(status) != 0))
+            return false;
 
     va_end(args);
 
